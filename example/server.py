3951713dea8a58c6c973de2563fe933dc1f34c27
karech <karech@users.noreply.github.com>
2022-02-13 18:59:54 +0300

    update

---
+++ example/server.py
@@ -0,0 +1,56 @@
+import logging
+from typing import Optional
+
+import uvicorn
+from pydantic import BaseModel
+
+from github_webhooks import create_app
+from github_webhooks.schemas import WebhookCommonPayload
+
+
+class PullPayload(WebhookCommonPayload):
+    class Pull(BaseModel):
+        title: str
+        url: str
+
+    action: str
+    pull_request: Pull
+
+
+class CommentPayload(WebhookCommonPayload):
+    class Comment(BaseModel):
+        url: str
+        body: str
+
+    action: str
+    comment: Comment
+
+
+# Init the App
+app = create_app(secret_token='super-secret-token')
+
+
+# Register new handler via deco
+@app.hooks.register('pull_request', PullPayload)
+async def handle_pull_request(payload: PullPayload) -> Optional[str]:
+    logging.info(
+        'PR <%s> opened by <%s>\nlink: %s',
+        payload.pull_request.title,
+        payload.sender.login,
+        payload.pull_request.url,
+    )
+    return 'OK'
+
+
+# define new async function event handling
+async def handle_pull_request_review_comment(payload: CommentPayload) -> None:
+    logging.info('PR comment by <%s>: <%s>', payload.sender.login, payload.comment.body)
+
+
+# Register new handler via `add_handler` function
+app.hooks.add_handler('pull_request_review_comment', CommentPayload, handle_pull_request)
+
+
+if __name__ == '__main__':
+    # start uvicorn server
+    uvicorn.run(app)
--- github_webhooks/handlers/default.py
+++ github_webhooks/handlers/default.py
@@ -4,6 +4,6 @@ from typing import Optional
 from github_webhooks.schemas import WebhookHeaders
 
 
-async def handle_default(event: str, headers: WebhookHeaders, payload: bytes) -> Optional[str]:
+async def handle_default(event: str, payload: bytes, headers: WebhookHeaders) -> Optional[str]:
     logging.debug('Default handler for <%s> event, headers: %s, payload: %s', event, headers, payload)
     return 'OK'
